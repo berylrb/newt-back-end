13eb2b2ab81fe7f7644943b2f54fb449aacdc937
Kevin <kevin.rodriguez678@gmail.com>
2022-07-16 15:00:36 -0400

    add routes to profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -10,5 +10,7 @@ router.get('/', profilesCtrl.index)
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
+router.post('/:id/userActivities', checkAuth, profilesCtrl.create)
+router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 
 export { router }
