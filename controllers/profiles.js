f3ddaec028bf338fca1c303adfe95d822112364d
Kevin <kevin.rodriguez678@gmail.com>
2022-07-16 14:57:46 -0400

    add create function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -19,6 +19,22 @@ function show(req,res){
   })
 }
 
+function create(req, res){
+  req.body.owner = req.user.profile
+  createUserActivity.create(req.body)
+  .then(userActivity => {
+    userActivity.findById(userActivity._id)
+    .populate('owner')
+    .then(populatedUserActivity => {
+      res.json(populatedUserActivity)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
+}
+
 export { 
   index,
   show,
