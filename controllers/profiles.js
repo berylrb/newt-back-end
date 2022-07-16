c9ee8158c5bfe6e01091898e046cd95261ed7778
Kevin <kevin.rodriguez678@gmail.com>
2022-07-16 14:59:59 -0400

    try to fix merge issue

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -35,7 +35,28 @@ function create(req, res){
   })
 }
 
+function addPhoto(req, res) {
+  const imageFile = req.files.photo.path
+  Profile.findById(req.params.id)
+  .then(profile => {
+    cloudinary.uploader.upload(imageFile, {tags: `${profile.email}`})
+    .then(image => {
+      profile.photo = image.url
+      profile.save()
+      .then(profile => {
+        res.status(201).json(profile.photo)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+}
+
 export { 
   index,
   show,
+  create,
+  addPhoto,
 }
